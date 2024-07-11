6d70210c251427cce6830222860d379d289bd085
aroona89 <aroonaali899@gmail.com>
2024-07-11 15:46:21 +0500

    need to display 3 columns, so removed extra column

--- src/components/discoverNfts/DiscoverNfts.tsx
+++ src/components/discoverNfts/DiscoverNfts.tsx
@@ -48,7 +48,7 @@ export default function DiscoverNfts(): React.JSX.Element {
               </button>
             </div>
             <div className="container mx-auto p-4 bg-[#2B2B2B]">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
                 {visibleCollection.map((item: Collection) => (
                   <div
                     key={item.collection}
